8be5a711e5227b864334e080b2446664f9b5f76d
kiasambrook <kiasambrook@btinternet.com>
2022-02-11 11:06:04 +0000

    cpanel autodeploy

--- index.php
+++ index.php
@@ -22,6 +22,16 @@ if ( $sendflag == "send" )
 <!DOCTYPE html>
 <html lang="en">
   <head>
+   <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-KREZ925FZS"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-KREZ925FZS');
+</script>
+   
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta
